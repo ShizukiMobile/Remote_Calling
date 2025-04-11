5c773d583039546c0f2b7555188dc4ed36ebef32
しづきモバイル <shizuki0918@outlook.jp>
2025-04-11 12:03:56 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const { Server } = require("socket.io");
-const io = new Server({
-  cors: { origin: "*" }
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
 });
 
-io.on("connection", (socket) => {
-  socket.on("join", (room) => {
-    socket.join(room);
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId);
+    console.log(`${socket.id} joined room ${roomId}`);
   });
 
-  socket.on("call", (room) => {
-    socket.to(room).emit("incoming-call");
+  socket.on('call', (roomId) => {
+    socket.to(roomId).emit('call');  // 自分以外の同じルームの人に通知
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
   });
 });
 
-io.listen(10000); // Renderではポート10000を使うとよい
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
