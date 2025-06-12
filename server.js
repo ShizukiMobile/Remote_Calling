d4e073d65768145c9f30781a687c93b27b967051
しづきモバイル <shizuki0918@outlook.jp>
2025-06-12 20:46:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+
+const app = express();
 const cors = require('cors');
 app.use(cors({
   origin: 'https://shizukimobile.github.io'
 }));
-
-const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
